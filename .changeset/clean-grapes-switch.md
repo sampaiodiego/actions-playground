86cc88661326ca625ecdc1c6addb9caeacc93aa4
Diego Sampaio <chinello@gmail.com>
2023-05-16 15:04:30 -0300

    fix: doing a patch thx

---
+++ .changeset/clean-grapes-switch.md
@@ -0,0 +1,5 @@
+---
+"@diegosampaio/backend": patch
+---
+
+how this patch will impact the version?
--- apps/backend/index.js
+++ apps/backend/index.js
@@ -2,6 +2,7 @@
 
 // fix nothing
 
+// add patch after minor nro 2
 
 // minor nro 1
 // minor nro 2
