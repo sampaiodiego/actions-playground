80a42aaef091009888df70f612e301142a26ebea
Diego Sampaio <chinello@gmail.com>
2023-06-09 17:38:03 -0300

    get pr body

--- packages/release-action/src/cutFinalRelease.ts
+++ packages/release-action/src/cutFinalRelease.ts
@@ -6,6 +6,7 @@ import * as github from '@actions/github';
 
 import { createNpmFile } from './createNpmFile';
 import { setupOctokit } from './setupOctokit';
+import { getChangelogEntry } from './utils';
 
 export async function cutFinalRelease({ githubToken, cwd = process.cwd() }: { githubToken: string; cwd?: string }) {
 	const octokit = setupOctokit(githubToken);
@@ -30,10 +31,27 @@ export async function cutFinalRelease({ githubToken, cwd = process.cwd() }: { gi
 	// bump version of all packages to rc
 	await exec('yarn', ['changeset', 'version']);
 
-	// get version from main package
-	const { version: newVersion } = require(path.resolve(cwd, 'apps', 'backend', 'package.json'));
 
-	// TODO get changelog from main package and copy to root package
+	const mainPackagePath = path.join(cwd, 'apps', 'backend');
+
+	// get version from main package
+	const mainPackageJsonPath = path.join(mainPackagePath, 'package.json');
+	const { version: newVersion } = require(mainPackageJsonPath);
+
+	const mainPackageChangelog = path.join(mainPackagePath, 'CHANGELOG.md');
+
+	const changelogContents = fs.readFileSync(
+		mainPackageChangelog,
+		'utf8'
+	);
+	const changelogEntry = getChangelogEntry(changelogContents, newVersion);
+	if (!changelogEntry) {
+		// we can find a changelog but not the entry for this version
+		// if this is true, something has probably gone wrong
+		throw new Error('Could not find changelog entry for version newVersion');
+	}
+
+	const releaseBody = changelogEntry.content;
 
 	// update root package.json
 	const rootPackageJsonPath = path.resolve(cwd, "package.json");
@@ -57,7 +75,7 @@ export async function cutFinalRelease({ githubToken, cwd = process.cwd() }: { gi
 	await octokit.rest.repos.createRelease({
 		name: newVersion,
 		tag_name: newVersion,
-		body: '// TODO get changelog \nnew release body',
+		body: releaseBody,
 		prerelease: newVersion.includes("-"),
 		...github.context.repo,
 	});
