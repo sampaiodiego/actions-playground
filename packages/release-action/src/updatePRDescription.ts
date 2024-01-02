d886868b7d1ee20839c99a36269e1c1a8f10549f
Diego Sampaio <chinello@gmail.com>
2024-01-02 16:54:10 -0300

    clean body

--- packages/release-action/src/updatePRDescription.ts
+++ packages/release-action/src/updatePRDescription.ts
@@ -22,24 +22,14 @@ export async function updatePRDescription({
 	// generate change logs from changesets
 	await exec('yarn', ['changeset', 'version']);
 
-	console.log('mainPackagePath ->', mainPackagePath);
 
 	// get version from main package
 	const { version: newVersion } = await readPackageJson(mainPackagePath);
 
-	// console.log('newVersion ->', newVersion);
-
 	const mainPackageChangelog = path.join(mainPackagePath, 'CHANGELOG.md');
 
-	// console.log('mainPackageChangelog ->', mainPackageChangelog);
-
 	const changelogContents = fs.readFileSync(mainPackageChangelog, 'utf8');
-
-	// console.log('changelogContents ->', changelogContents);
-
 	const changelogEntry = getChangelogEntry(changelogContents, newVersion);
-
-	console.log('changelogEntry ->', changelogEntry);
 	if (!changelogEntry) {
 		// we can find a changelog but not the entry for this version
 		// if this is true, something has probably gone wrong
@@ -48,8 +38,6 @@ export async function updatePRDescription({
 
 	const releaseBody = (await getEngineVersionsMd(cwd)) + changelogEntry.content;
 
-	// console.log('releaseBody ->', releaseBody);
-
 	core.info('get PR description');
 	const result = await octokit.rest.pulls.get({
 		pull_number: github.context.issue.number,
@@ -57,17 +45,22 @@ export async function updatePRDescription({
 		...github.context.repo,
 	});
 
-	console.log('result.data.body ->', result.data.body);
+	const { body: originalBody = '' } = result.data;
 
-	const oldBody = result.data.body?.replace(/<!-- release-notes-start -->.*<!-- release-notes-end -->/s, '').trim();
+	const cleanBody = originalBody?.replace(/<!-- release-notes-start -->.*<!-- release-notes-end -->/s, '').trim();
 
-	console.log('oldBody ->', oldBody);
+	const bodyUpdated = `${cleanBody}\n\n<!-- release-notes-start -->\n# ${newVersion}\n\n${releaseBody}\n<!-- release-notes-end -->`;
+
+	if (bodyUpdated == originalBody) {
+		core.info('no changes to PR description');
+		return;
+	}
 
 	core.info('update PR description');
 	await octokit.rest.pulls.update({
 		owner: github.context.repo.owner,
 		repo: github.context.repo.repo,
 		pull_number: github.context.issue.number,
-		body: `${oldBody}\n\n<!-- release-notes-start -->\n# ${newVersion}\n\n${releaseBody}\n<!-- release-notes-end -->`,
+		body: bodyUpdated,
 	});
 }
